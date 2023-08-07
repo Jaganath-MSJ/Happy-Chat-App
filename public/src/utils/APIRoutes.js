1851202319175eda876682b98c36c4c8d2656b00
Jaganath M S <jagan.msjc@gmail.com>
2023-08-07 11:24:04 +0530

    port change

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:8000";
+export const host = "https://happy-chat-backend.onrender.com";
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const setAvatarRoute = `${host}/api/auth/setAvatar`;
